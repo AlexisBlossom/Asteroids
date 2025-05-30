a1f318071ba6aba5b5819836fbe4c16b149569db
AlexisBlossom <ajtauer00@gmail.com>
2025-05-30 13:30:57 -0400

    added player movement controls, added player to screen, now reads inputs, FPS limited to 60

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame,constants
+from player import Player
 from constants import *
 def main():
     pygame.init()
-    print("Starting Asteroids!\nScreen width: 1280\nScreen height: 720")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    
-    
+        player.update(dt)
+# limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
+
+
 if __name__ == "__main__":
     main()
